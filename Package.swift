2ec3a7d9a4792a54a63845d28e8dde63cccf57d0
ozitrance <ozyair85@gmail.com>
2019-10-23 20:33:15 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,4 +11,17 @@ let package = Package(
   //      .package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 1),
     ]
 //    swiftLanguageVersions: [3, 4]
+    ,
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "PCA9685",
+            dependencies: []),
+           // dependencies: ["SwiftyGPIO", "SwiftyServo"]),
+         //   dependencies: ["SwiftyGPIO"]),
+        .testTarget(
+            name: "PCA9685Tests",
+            dependencies: ["PCA9685"]),
+    ]
 )
